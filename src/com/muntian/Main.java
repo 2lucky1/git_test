35c8051d653571fdefd940cff7b6accea7c5cc54
Mykola_Muntian <mmuntian@luxoft.com>
2020-03-26 12:03:03 +0200

    Added sout in main

--- src/com/muntian/Main.java
+++ src/com/muntian/Main.java
@@ -3,6 +3,7 @@ package com.muntian;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello git");
+        System.out.println("Hello git");
     }
 }
