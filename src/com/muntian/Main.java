e9ae69c442c764d94a5301766ec1c04c0bb6f08e
Mykola_Muntian <mmuntian@luxoft.com>
2020-03-26 14:14:44 +0200

    changes from master

--- src/com/muntian/Main.java
+++ src/com/muntian/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello git");
-        System.out.println("Hello git 3");
+        System.out.println("Hello git 4");
         System.out.println("Hello git 5");
     }
 }
