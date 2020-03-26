529a271af510d9d4287db8f79181b35977761121
Mykola_Muntian <mmuntian@luxoft.com>
2020-03-26 13:41:46 +0200

    M5

--- src/com/muntian/Main.java
+++ src/com/muntian/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello git");
         System.out.println("Hello git");
         System.out.println("Hello git 3");
+        System.out.println("Hello git 5");
     }
 }
