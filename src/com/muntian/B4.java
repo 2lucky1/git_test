2a30c68432c63bfe422b91dfb1e9bc5104389b31
Mykola_Muntian <mmuntian@luxoft.com>
2020-03-27 13:21:50 +0200

    -mM3_change

--- src/com/muntian/B4.java
+++ src/com/muntian/B4.java
@@ -4,5 +4,6 @@ public class B4 {
     public static void main(String[] args) {
         System.out.println("change1");
         System.out.println("M_change2");
+        System.out.println("M_change3");
     }
 }
