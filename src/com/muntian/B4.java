89fd7970ba67e238acf11483cedd005ef8eedc2e
Mykola_Muntian <mmuntian@luxoft.com>
2020-03-27 12:59:32 +0200

    -mchange2 in B4

--- src/com/muntian/B4.java
+++ src/com/muntian/B4.java
@@ -2,7 +2,12 @@ package com.muntian;
 
 public class B4 {
     public static void main(String[] args) {
+
         System.out.println("change1");
         System.out.println("changes2");
+
+        System.out.println("B4 change1");
+        System.out.println("B4 change2");
+
     }
 }
