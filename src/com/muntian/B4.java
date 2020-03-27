379d2353ca44d426a0f2b70a3244bcf18ec5f0b7
Mykola_Muntian <mmuntian@luxoft.com>
2020-03-27 13:27:56 +0200

    revert M3

--- src/com/muntian/B4.java
+++ src/com/muntian/B4.java
@@ -4,7 +4,6 @@ public class B4 {
     public static void main(String[] args) {
         System.out.println("change1");
         System.out.println("M_change2");
-        System.out.println("M_change3");
         System.out.println("M_change4");
     }
 }
