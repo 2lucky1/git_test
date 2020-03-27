bf672d20cf37116b3e8b932ad85e8a794d62443f
Mykola_Muntian <mmuntian@luxoft.com>
2020-03-27 13:49:02 +0200

    changed Main

--- src/com/muntian/Main.java
+++ src/com/muntian/Main.java
@@ -14,5 +14,7 @@ public class Main {
 
         System.out.println("Master cherry pick");
         System.out.println("Master cherry pick 1 change");
+
+        System.out.println("M6_rev");
     }
 }
