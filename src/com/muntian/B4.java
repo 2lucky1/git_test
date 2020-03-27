0fc4c12771011fe808e90545a262da8009de44d7
Mykola_Muntian <mmuntian@luxoft.com>
2020-03-27 13:20:00 +0200

    -mclear

--- src/com/muntian/B4.java
+++ src/com/muntian/B4.java
@@ -4,10 +4,5 @@ public class B4 {
     public static void main(String[] args) {
 
         System.out.println("change1");
-        System.out.println("changes2");
-
-        System.out.println("B4 change1");
-        System.out.println("B4 change2");
-
     }
 }
