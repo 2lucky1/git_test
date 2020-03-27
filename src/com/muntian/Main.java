35543ea59d06eea41ddf64699f19d48f0c41cb8e
Mykola Muntian <lanikolya1989@gmail.com>
2020-03-27 11:35:40 +0200

    Update Main.java

--- src/com/muntian/Main.java
+++ src/com/muntian/Main.java
@@ -11,5 +11,6 @@ public class Main {
         System.out.println("b1 changes");
         System.out.println("Changes from GitHub");
         System.out.println("Second update from GitHub");
+        System.out.println("Third update from GitHub");
     }
 }
